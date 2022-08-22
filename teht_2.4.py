a30089785f7ab7082eff86ae0d6506fc4a0842e8
Fadak <thisisfadak@gmail.com>
2022-08-22 14:55:54 +0300

    4commit

---
+++ teht_2.4.py
@@ -0,0 +1,15 @@
+#syötteet
+luku_1 = int(input("Anna 1. kokonaisluku"))
+luku_2 = int(input("Anna 2. kokonaisluku"))
+luku_3 = int(input("Anna 3. kokonaisluku"))
+
+#Laskutoimitukset
+summa = luku_1 + luku_2 + luku_3
+tulo = luku_1 * luku_2 * luku_3
+ka= summa/3
+
+#tulostus
+print(summa)
+print(tulo)
+print(ka)
+
