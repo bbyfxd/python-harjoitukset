81bb5b1491649a264205ddd614df076cd0bd1e81
Fadak <thisisfadak@gmail.com>
2022-08-22 14:43:47 +0300

    eka commit

---
+++ teht_2.2.py
@@ -0,0 +1,10 @@
+import math
+
+r = float(input ("Anna ympyrän säde"))
+ala = math.pi * math.pow(r, 2)
+
+print(f"Ympyrän pinta-ala on {ala: 3.f})
+
+
+
+
---
+++ teht_2.3.py
@@ -0,0 +1,11 @@
+# kysytään tiedot
+kanta = float(input("Anna suorakulmion kanta"))
+korkeus = float(input("Anna suorakulmion korkeus"))
+
+# laskutoimitus
+A = kanta * korkeus
+piiri = (kanta * 2) + (korkeus * 2)
+
+#tulostukset
+print(f"Ala = {A}")
+print(f"Piiri = {piiri}")
