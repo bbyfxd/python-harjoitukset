3b054c4019055fd2772310a51b709042e2bec727
Fadak <thisisfadak@gmail.com>
2022-08-24 23:23:58 +0300

    4commit

--- teht_2.5.py
+++ teht_2.5.py
@@ -4,10 +4,12 @@ leiviskä = float(input("Anna leviskät:"))
 naula = float(input("Anna nauloja:"))
 luoti = float(input("Anna Luotoja:"))
 
-naula = leiviskä * 20
-luoti = naula * 32
-g =  luoti * 13.3
-kg = g / 1000
+vastaus1 = (leiviskä * 20) * 32 * 13.3
+vastaus2 = (naula * 32) * 13.3
+vastaus3 = luoti * 13.3
+Gramma = vastaus3 + vastaus2 + vastaus1
+KG = int(Gramma / 1000)
 
-loput = g-math.floor(kg) * 1000
-print(f"Massa nykyarvoilla: {kg:0.2f} {loput:0.2f}")
+left_overs = math.floor(Gramma % 1000)
+
+print(f"{KG} Kilo ja {left_overs} Gramma")
--- teht_2.6.py
+++ teht_2.6.py
@@ -1,7 +1,10 @@
 import random
 
 x = random.randint(0,9)
-print(x)
 
-x = random.randint(1,6)
-print(x)
+x = random.randint(9,0)
+
+x = random.randint(0,9)
+
+print(x + x + x)
+
