aedec033febca11b444d1bd49be42579ce831f69
Fadak <thisisfadak@gmail.com>
2022-08-24 23:31:20 +0300

    4commit

--- teht_2.6.py
+++ teht_2.6.py
@@ -1,10 +1,13 @@
 import random
 
 x = random.randint(0,9)
+y = random.randint(0,9)
+z = random.randint(0,9)
+print(x,y,z)
 
-x = random.randint(9,0)
-
-x = random.randint(0,9)
-
-print(x + x + x)
+a = random.randint(1,6)
+b = random.randint(1,6)
+c = random.randint(1,6)
+d = random.randint(1,6)
+print(a,b,c,d)
 
